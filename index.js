edd7191cb1773e24f2ed48133d9789f8dcd6d3c9
mochan_tk <takashi1.kawamoto@gmail.com>
2023-02-06 16:15:30 +0000

    commit

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ async function handleEvent(event) {
 
     // create a echoing text message
     // const echo = { type: 'text', text: event.message.text };
-    const echo = { type: 'text', text: 'success2ß' };
+    const echo = { type: 'text', text: 'success2' };
 
     // use reply API
     return client.replyMessage(event.replyToken, echo);
