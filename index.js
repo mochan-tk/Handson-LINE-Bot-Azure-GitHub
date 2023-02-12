dc90e68857b4e8ab6b6425d6579ac0584ae0b789
mochan_tk <takashi1.kawamoto@gmail.com>
2023-02-12 05:17:07 +0000

    commit

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ const getStreamData = async (stream)  => {
         result.push(Buffer.from(chunk));
       });
       stream.on("end", () => {
-        resolve(result);
+        resolve(Buffer.concat(result));
       });
     });
 }
